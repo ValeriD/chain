4ba843ace180806846c7a450da2f80ae39fc03bc
Valeri <valer.dobrev10@gmail.com>
2021-11-11 10:29:05 +0200

    Created loading component

---
+++ src/views/dashboard/components/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row>
+    <img
+      class="img"
+      :src='loadingImg'
+    />
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+
+  props:{
+    loadingImg:{
+        type:String,
+        default: require("@/assets/loading.gif"),
+    }
+  },
+  data: () => ({ 
+      isLoading:true,
+  }),
+};
+</script>
+
+<style>
+.img{
+  position: absolute;
+  height: 50px;
+  width: 50px;
+  top: 50%;
+  left: 50%;
+}
+</style>
